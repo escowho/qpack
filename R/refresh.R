641125c8173dc7ba0fb0d5ffe6d8e2e6ed467dd8
mshefferq <92955491+mshefferq@users.noreply.github.com>
2022-02-09 13:28:22 -0600

    Adding dev install option to refresh

--- R/refresh.R
+++ R/refresh.R
@@ -1,16 +1,31 @@
 #' @title Refreshes github install of qpack
 #' @description Convenience function to perform a remotes::install_github for qpack.
 #' @param pack_list A character vector with the names of packages to update. Default: qpack.
+#' @param dev A logical to indicate if the development version of qpack should be
+#' installed.  Will only work if qpack is the only package listed in the pack_list.
+#' Default: FALSE.
 #' @return Nothing, updates package(s).
 #' @examples
 #' \dontrun{
 #' refresh()
-#' refresh(c("qpack"))
+#' refresh(dev=TRUE)
 #' }
 #' @export
 #' @importFrom remotes install_github
 
-refresh <- function(pack_list="qpack"){
+refresh <- function(pack_list="qpack", dev=FALSE){
+
+  # Helper Functions --------------------------------------------------------
+
+  update_pack <- function(pack){
+    remotes::install_github(paste0("mshefferq/", pack))
+  }
+
+  update_dev <- function(pack){
+    remotes::install_github("mshefferq/qpack", ref="dev")
+  }
+
+  # Function ----------------------------------------------------------------
 
   if(missing(pack_list)){
     if (Sys.getenv("QPACK_SETUP_PACKS")==""){
@@ -21,9 +36,12 @@ refresh <- function(pack_list="qpack"){
     }
   }
 
-  update_pack <- function(pack){
-    remotes::install_github(paste0("mshefferq/", pack))
+  if (dev==TRUE & length(pack_list)==1 & pack_list=="qpack"){
+    invisible(update_dev())
+  } else if (dev==TRUE & length(pack_list)>1){
+    stop(call. = FALSE, "dev cannot be TRUE if any pack other than qpack is listed in the pack_list.")
+  } else {
+    invisible(lapply(pack_list, update_pack))
   }
 
-  invisible(lapply(pack_list, update_pack))
 }
